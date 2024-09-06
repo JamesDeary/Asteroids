bf9d3857620139d9e2fbb97f65638f4f120960ac
JamesDeary <jamiedeary@gmail.com>
2024-09-06 23:49:31 +0100

    Created the 'player', all that is left is the asteroids...

--- main.py
+++ main.py
@@ -3,19 +3,44 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     print("Starting asteroids!")
     print(f"Screen width:",SCREEN_WIDTH)
     print(f"Screen height:",SCREEN_HEIGHT)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Player Triangle Test")
+
+    player = Player(x= SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2, radius = PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0,0,0))
+        
+        for thing in updateable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        
+    pygame.quit()
+
 if __name__ == "__main__":
     main()
